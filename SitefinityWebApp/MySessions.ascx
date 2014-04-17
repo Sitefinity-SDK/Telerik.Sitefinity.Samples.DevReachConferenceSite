d50440202297bb31fd9d3b4d276db522191f5a5f
unknown <smilenov@SMILENOV.telerik.com>
2014-04-17 17:57:39 +0300

    Initial commit of DevReach project sample

---
+++ SitefinityWebApp/MySessions.ascx
@@ -0,0 +1,34 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="MySessions.ascx.cs" Inherits="SitefinityWebApp.MySessions" %>
+<%@ Import Namespace="System.Data.Common" %>
+<%@ Import Namespace="System.Data" %>
+<%@ Import Namespace="Telerik.Sitefinity.RelatedData" %>
+
+<table class="list-sessions">
+    <asp:Repeater runat="server" ID="SessionsRepeater">
+        <ItemTemplate>
+            <tr>
+                <td>
+                    <h3>
+                        <%# Eval("Title")%>
+                        <span>
+                            <asp:Repeater runat="server" ID="speakers" DataSource='<%# Eval("Speakers")%>'>
+                                <ItemTemplate>
+                                    <%# Eval("Title") %>, 
+                                </ItemTemplate>
+                            </asp:Repeater>
+                        </span>
+                    </h3>
+                </td>
+                <td><%# Eval("Location.Title")%></td>
+                <td class="attended">Attended by <span runat="server"><%# Eval("CurrentAttendees")%></span></td>
+                <td>
+                    <asp:Button ID="click" class='<%# CheckIsAttended(Container.DataItem)?"small positive":"small negative" %>' runat="server"
+                        Text='<%# CheckIsAttended(Container.DataItem)?"Attending":"Will Attend" %>'
+                        CommandName='<%# CheckIsAttended(Container.DataItem)?"NotAttend":"Attend" %>'
+                        OnCommand="CommandBtn_Click"
+                        CommandArgument='<%# Eval("OriginalContentId") %>' />
+                <td>
+            </tr>
+        </ItemTemplate>
+    </asp:Repeater>
+</table>
---
+++ SitefinityWebApp/MySessions.ascx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI.WebControls;
+using Telerik.Sitefinity.Data.ContentLinks;
+using Telerik.Sitefinity.DynamicModules;
+using Telerik.Sitefinity.DynamicModules.Model;
+using Telerik.Sitefinity.Model;
+using Telerik.Sitefinity.Model.ContentLinks;
+using Telerik.Sitefinity.RelatedData;
+using Telerik.Sitefinity.Security;
+using Telerik.Sitefinity.Security.Model;
+using Telerik.Sitefinity.Utilities.TypeConverters;
+
+namespace SitefinityWebApp
+{
+    public partial class MySessions : System.Web.UI.UserControl
+    {
+        private const string SessionsType ="Telerik.Sitefinity.DynamicTypes.Model.DevReach.Session";
+        private IList<ContentLink> mySessions = null;
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            var manager = DynamicModuleManager.GetManager();
+            var repeater = this.Controls.OfType<Repeater>().First();
+            var clManager = ContentLinksManager.GetManager();
+
+            mySessions = clManager.GetContentLinks().Where(link => 
+                link.ChildItemType == typeof(User).FullName && 
+                link.ChildItemId == SecurityManager.CurrentUserId && 
+                link.ParentItemType == SessionsType).ToList();
+        
+            var items = manager.GetDataItems(TypeResolutionService.ResolveType(SessionsType)).Where(item => item.Status == Telerik.Sitefinity.GenericContent.Model.ContentLifecycleStatus.Live);
+            
+            repeater.DataSource = items;
+            repeater.DataBind();
+        }
+
+        protected void CommandBtn_Click(Object sender, CommandEventArgs e)
+        {
+            var sessionId = Guid.Parse(e.CommandArgument.ToString());
+            var dynamicModuleManager = DynamicModuleManager.GetManager();
+            var myType = TypeResolutionService.ResolveType(SessionsType);
+
+            var masterItem = dynamicModuleManager.GetDataItem(myType, sessionId);
+            var liveItem = dynamicModuleManager.GetDataItems(myType).FirstOrDefault(item => item.OriginalContentId == sessionId && item.Status == Telerik.Sitefinity.GenericContent.Model.ContentLifecycleStatus.Live);
+            
+            if (e.CommandName == "Attend")
+            {
+                liveItem.CreateRelation(SecurityManager.CurrentUserId, null, typeof(User).FullName, "Attendees");
+                var currentAttendeesCount = liveItem.GetValue<decimal>("CurrentAttendees");
+                liveItem.SetValue("CurrentAttendees", currentAttendeesCount + 1);
+                masterItem.SetValue("CurrentAttendees", currentAttendeesCount + 1);
+            }
+            else
+            {
+                liveItem.DeleteRelation(SecurityManager.CurrentUserId, null, typeof(User).FullName, "Attendees");
+                var currentAttendeesCount = liveItem.GetValue<decimal>("CurrentAttendees");
+                if (currentAttendeesCount > 0)
+                {
+                    liveItem.SetValue("CurrentAttendees", currentAttendeesCount - 1);
+                    masterItem.SetValue("CurrentAttendees", currentAttendeesCount - 1);
+                }
+            }
+
+            dynamicModuleManager.SaveChanges();
+        }
+
+        protected bool CheckIsAttended(object item)
+        {
+            DynamicContent data = (DynamicContent)item;
+            var itemId = data.OriginalContentId;
+
+            if (mySessions.Count == 0) 
+                return false;
+            
+            return mySessions.FirstOrDefault(link => link.ParentItemId == itemId) != null;
+        }
+    }
+}
